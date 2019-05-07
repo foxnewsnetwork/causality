49c4e861d382abfadf5a71d4ed9206bf594ecc99
Thomas Chen <tom.chen@am.sony.com>
2019-05-06 17:10:03 -0700

    introduce sensible types for probability and variables associated

---
+++ src/utils/probability.ts
@@ -0,0 +1,21 @@
+export type Variable<Enum> = {
+  name: string,
+  domain: Enum
+}
+export type Dependencies<Enum> = Record<Variable<Enum>["name"], Variable<Enum>["domain"]>;
+
+export type Distribution<Enum> = Iterable<ProbabilityTableEntry<Enum>>
+
+export type ProbabilityTableEntry<Enum> = {
+  event: Enum,
+  probability: Probability
+}
+
+// Number between 0 and 1
+export type Probability = number;
+
+export type ProbabilityTable<Enum, V extends Variable<Enum>> = Record<V["name"], Distribution<Enum>>
+
+export type StochasticEquation<Enum> = () => Distribution<Enum>
+
+export type StructuralEquation<Enum> = (parents: Dependencies<any>) => Enum
