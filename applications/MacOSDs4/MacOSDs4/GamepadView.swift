b39b942e58b9fdc2a50aa9d2644af04b2dbf8087
Thomas Chen <foxnewsnetwork@gmail.com>
2022-01-06 19:15:23 -0800

    scaffold in swift stuff for emulating ds4

---
+++ applications/MacOSDs4/MacOSDs4/GamepadView.swift
@@ -0,0 +1,21 @@
+//
+//  GamepadView.swift
+//  MacOSDs4
+//
+//  Created by Thomas Chen on 12/22/21.
+//
+
+import SwiftUI
+
+struct GamepadView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct GamepadView_Previews: PreviewProvider {
+    static var previews: some View {
+        GamepadView()
+    }
+}
---
+++ applications/MacOSDs4/MacOSDs4/MacOSDs4App.swift
@@ -0,0 +1,17 @@
+//
+//  MacOSDs4App.swift
+//  MacOSDs4
+//
+//  Created by Thomas Chen on 12/22/21.
+//
+
+import SwiftUI
+
+@main
+struct MacOSDs4App: App {
+    var body: some Scene {
+        WindowGroup {
+            GamepadView()
+        }
+    }
+}
